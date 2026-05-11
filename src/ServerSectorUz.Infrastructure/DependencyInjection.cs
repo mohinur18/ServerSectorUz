29d636ed1e64c2cf071766665e7f2941236876c1
Mohinur <muhammadjonovamohinur36@gmail.com>
2026-05-11 16:05:56 +0500

    Register expense services

--- src/ServerSectorUz.Infrastructure/DependencyInjection.cs
+++ src/ServerSectorUz.Infrastructure/DependencyInjection.cs
@@ -7,20 +7,21 @@ using ServerSectorUz.Core.Brokers.Securities;
 using ServerSectorUz.Core.Brokers.Storages;
 using ServerSectorUz.Core.Brokers.Tokens;
 using ServerSectorUz.Core.Models.Configurations;
-using ServerSectorUz.Core.Services.Foundations.AuthenticationUsers;
 using ServerSectorUz.Core.Services.Foundations.Attendance;
+using ServerSectorUz.Core.Services.Foundations.AuthenticationUsers;
 using ServerSectorUz.Core.Services.Foundations.HR;
 using ServerSectorUz.Core.Services.Foundations.RolesPermissions;
 using ServerSectorUz.Core.Services.Foundations.Salaries;
 using ServerSectorUz.Core.Services.Foundations.Tasks;
 using ServerSectorUz.Core.Services.Orchestrations.AuthenticationUsers;
-using ServerSectorUz.Infrastructure.Brokers.Securities;
 using ServerSectorUz.Infrastructure.Brokers.DateTimes;
 using ServerSectorUz.Infrastructure.Brokers.Loggings;
+using ServerSectorUz.Infrastructure.Brokers.Securities;
 using ServerSectorUz.Infrastructure.Brokers.Storages;
 using ServerSectorUz.Infrastructure.Brokers.Tokens;
-using ServerSectorUz.Infrastructure.Services.Foundations.AuthenticationUsers;
 using ServerSectorUz.Infrastructure.Services.Foundations.Attendance;
+using ServerSectorUz.Infrastructure.Services.Foundations.AuthenticationUsers;
+using ServerSectorUz.Infrastructure.Services.Foundations.Expenses;
 using ServerSectorUz.Infrastructure.Services.Foundations.HR;
 using ServerSectorUz.Infrastructure.Services.Foundations.RolesPermissions;
 using ServerSectorUz.Infrastructure.Services.Foundations.Salaries;
@@ -64,6 +65,9 @@ public static class DependencyInjection
         services.AddTransient<IEmployeeSalaryService, EmployeeSalaryService>();
         services.AddTransient<ITaskItemService, TaskItemService>();
         services.AddTransient<IAuthenticationOrchestrationService, AuthenticationOrchestrationService>();
+
+        services.AddTransient<IExpenseClaimService, ExpenseClaimService>();
+        services.AddTransient<IExpenseItemService, ExpenseItemService>();
 
         return services;
     }
